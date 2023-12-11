1914c3299766190d3844799ad5bf50f72631b14f
Debabrata Patnaik <debabrata_patnaik@live.com>
2023-12-11 22:52:52 +0530

    Initial commit for multipart testing with String value and file MultipartFile[] file

---
+++ src/main/java/org/doogle/multipartspring/MultipartController.java
@@ -0,0 +1,32 @@
+package org.doogle.multipartspring;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class MultipartController {
+
+  @PostMapping(path = "/multipart",
+      consumes = MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Map<String, String> multipart(@RequestParam String jsonPayload,
+      @RequestPart MultipartFile[] files) {
+    return Map.of("fileName", String.join(": ", "File received is : ", files[0].getName()));
+  }
+
+  @PostMapping(path = "/test", produces = APPLICATION_JSON_VALUE, consumes = MULTIPART_FORM_DATA_VALUE)
+  public Map<String, String> test(@RequestParam String payload, HttpServletRequest request) {
+    return Map.of("text", "Hello " + payload);
+  }
+
+}
---
+++ src/main/java/org/doogle/multipartspring/MultipartSpringApplication.java
@@ -0,0 +1,13 @@
+package org.doogle.multipartspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultipartSpringApplication.class, args);
+  }
+
+}
